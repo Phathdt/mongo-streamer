0cf925723e7ffc91c2784806a6d1d8440d67af2d
Phathdt <phathdt379@gmail.com>
2023-12-30 12:03:35 +0700

    resume after stop

--- streamer/streamer.go
+++ streamer/streamer.go
@@ -7,6 +7,7 @@ import (
 	sctx "github.com/phathdt/service-context"
 	"github.com/pkg/errors"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"mongo-streamer/plugins/mongoc"
@@ -19,6 +20,23 @@ type streamer struct{}
 
 func New() *streamer {
 	return &streamer{}
+}
+
+type ResumeToken struct {
+	ID    primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
+	Token string             `json:"token" bson:"token"`
+}
+
+type ChangeDocument struct {
+	ID struct {
+		Data string `json:"_data" bson:"_data"`
+	} `json:"_id" bson:"_id"`
+	DocumentKey struct {
+		ID string `json:"_id" bson:"_id"`
+	} `json:"document_key" bson:"documentKey"`
+	FullDocument             map[string]interface{} `json:"full_document" bson:"fullDocument"`
+	FullDocumentBeforeChange map[string]interface{} `json:"full_document_before_change" bson:"fullDocumentBeforeChange"`
+	OperationType            string                 `json:"operation_type" bson:"operationType"`
 }
 
 func (s *streamer) Run(sc sctx.ServiceContext) error {
@@ -35,7 +53,19 @@ func (s *streamer) Run(sc sctx.ServiceContext) error {
 
 	resumeTokenDb := comp.GetClient().Database(fmt.Sprintf("%s-resume-tokens", comp.GetDbName()))
 	resumeTokenCollection := resumeTokenDb.Collection(comp.GetCollectionName())
-	opts := options.ChangeStream().SetFullDocument("updateLookup").SetFullDocumentBeforeChange("whenAvailable")
+
+	// Check if the resumeToken is already stored
+	resumeToken := getStoredResumeToken(resumeTokenCollection)
+
+	// Create a Change Stream with resumeAfter
+	opts := options.ChangeStream().
+		SetFullDocument("updateLookup").
+		SetFullDocumentBeforeChange("whenAvailable")
+
+	if resumeToken != nil {
+		opts = opts.SetResumeAfter(bson.D{{"_data", resumeToken}})
+	}
+
 	stream, err := collection.Watch(ctx, mongo.Pipeline{}, opts)
 	if err != nil {
 		return err
@@ -44,19 +74,7 @@ func (s *streamer) Run(sc sctx.ServiceContext) error {
 	for stream.Next(ctx) {
 		bsonRaw := stream.Current
 
-		// Unmarshal BSON to JSON
-		type changeset struct {
-			ID struct {
-				Data string `json:"_data" bson:"_data"`
-			} `json:"_id" bson:"_id"`
-			DocumentKey struct {
-				ID string `json:"_id" bson:"_id"`
-			} `json:"document_key" bson:"documentKey"`
-			FullDocument             map[string]interface{} `json:"full_document" bson:"fullDocument"`
-			FullDocumentBeforeChange map[string]interface{} `json:"full_document_before_change" bson:"fullDocumentBeforeChange"`
-			OperationType            string                 `json:"operation_type" bson:"operationType"`
-		}
-		var bsonMap changeset
+		var bsonMap ChangeDocument
 		if err = bson.Unmarshal(bsonRaw, &bsonMap); err != nil {
 			return err
 		}
@@ -80,10 +98,25 @@ func (s *streamer) Run(sc sctx.ServiceContext) error {
 }
 
 func storeResumeToken(resumeToken string, collection *mongo.Collection) error {
-	doc := bson.M{"token": resumeToken}
+	doc := ResumeToken{Token: resumeToken}
 	if _, err := collection.InsertOne(context.Background(), doc); err != nil {
 		return errors.WithStack(err)
 	}
 
 	return nil
+}
+
+// Function to get the stored resumeToken
+func getStoredResumeToken(collection *mongo.Collection) *string {
+	opts := options.FindOne().SetSort(bson.D{{"_id", -1}})
+	var result ResumeToken
+
+	err := collection.FindOne(context.Background(), bson.M{}, opts).Decode(&result)
+	if errors.Is(err, mongo.ErrNoDocuments) {
+		return nil
+	} else if err != nil {
+		return nil
+	}
+
+	return &result.Token
 }
